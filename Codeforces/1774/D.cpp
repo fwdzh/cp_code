74bdea10efafe7e4411de22dddecea9dc04b181a
fwdzh <wu_yi_@outlook.com>
2025-01-06 14:18:48 +0800

    写了第二题,CF1774D,st[j].erase(st[j].begin())把j下标忘写了,找了15分钟,本来15分钟能写完的

--- Codeforces/1774/D.cpp
+++ Codeforces/1774/D.cpp
@@ -37,7 +37,60 @@ constexpr ll LNF = 1000000000000000000LL;
 
 void solve()
 {
-    
+    /*
+    一个数量大于x的一定可以和一个小于x的进行交换
+
+    */
+    int n, m;
+    cin >> n >> m;
+    vector<vi> a(n + 1, vi(m + 1));
+    vi cnt(n + 1);
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+            cnt[i] += a[i][j];
+            sum += a[i][j];
+        }
+    if (sum % n) {
+        cout << "-1\n";
+        return;
+    }
+    sum /= n;
+    vector<set<int>> st(m + 1); // 记录这一列有0 且总数小于平均值
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (a[i][j] == 0 && cnt[i] < sum)
+                st[j].insert(i);
+        }
+    }
+    // cerr << sum << "\n";
+    vector<array<int, 3>> ans;
+    for (int i = 1; i <= n; i++) {
+        if (cnt[i] <= sum)
+            continue;
+        for (int j = 1; j <= m && cnt[i] > sum; j++) {
+            if (a[i][j]) {
+                while (!st[j].empty() && cnt[*st[j].begin()] == sum)
+                    st[j].erase(st[j].begin());
+                if (!st[j].empty()) {
+                    // swap(a[i][j], a[*st[j].begin()][j]);
+                    ans.push_back({ i, *st[j].begin(), j });
+                    cnt[*st[j].begin()]++;
+                    st[j].erase(st[j].begin());
+                    cnt[i]--;
+                }
+            }
+        }
+        // assert(cnt[i] == sum);
+    }
+    // dbg(a);
+    // for(int i=1;i<=n;i++)
+    //     for(int j=1;j<=n;j++)
+    //         cerr<<a[i][j]<<" \n"[j==n];
+    cout << sz(ans) << '\n';
+    for (auto [i, j, k] : ans)
+        cout << i << " " << j << " " << k << '\n';
 }
 
 int main()
