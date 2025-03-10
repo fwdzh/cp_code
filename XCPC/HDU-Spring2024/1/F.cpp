3a10f13a65c6295f36ab052167c09920effd613a
fwdzh <wu_yi_@outlook.com>
2025-03-10 11:37:13 +0800

    ...

--- XCPC/HDU-Spring2024/1/F.cpp
+++ XCPC/HDU-Spring2024/1/F.cpp
@@ -13,47 +13,31 @@ void solve()
         for (int j = 0; j < 3; j++)
             cin >> a[i][j];
     }
-    // 123 132
-    // 213 231
-    // 312 321
     map<int, int> mp;
     auto work = [&](array<int, 3> x, int num) {
         vector<int> p { 0, 1, 2 };
-        // map<int, int> nmp = mp;
         do {
-            // ux+v=w
-            // x=(w-v)/u
+
             int u = x[p[0]], v = x[p[1]], w = x[p[2]];
             if (((w - v) % u != 0) || (w - v) / u < 0)
                 continue;
             int X = (w - v) / u;
-            if (num && mp.contains(X)) {
+            if (num && mp.find(X)!=mp.end()) {
                 if (mp[X] == num + 1)
                     continue;
                 if (mp[X] == num)
                     mp[X]++;
             } else {
-                // nmp[X]=1;
-                if (mp.contains(X))
+                if (mp.find(X)!=mp.end())
                     continue;
                 mp[X] = 1;
             }
-            // cerr<<X<<" "<<nmp[X]<<'\n';
         } while (next_permutation(p.begin(), p.end()));
-        // for (auto it = nmp.begin(); it != nmp.end(); it++) {
-        //     // cerr<<it->first<<" "<<it->second<<"\n";
-        //     if (it->second != num + 1) {
-        //         nmp.erase(it);
-        //     }
-        // }
-        // mp = nmp;
-        // assert(mp.size()>0);
-        map<int,int>nmp;
-        for(auto [x,y]:mp){
-            if(y!=num+1) continue;
-            nmp[x]=y;
+        for (map<int,int>::iterator it = mp.begin(); it != mp.end(); ) {
+            if (it->second != num + 1)
+                it=mp.erase(it);
+            else ++it;
         }
-        mp=nmp;
     };
     for (int i = 0; i < n; i++) {
         work(a[i], i);
