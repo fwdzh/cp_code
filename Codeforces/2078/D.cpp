60da42188b4383c12981b644836ee38171b6016b
fwdzh <wu_yi_@outlook.com>
2025-03-11 14:11:42 +0800

    ...

--- Codeforces/2078/D.cpp
+++ Codeforces/2078/D.cpp
@@ -1,55 +1,42 @@
 // Date: 2025-03-11
-// Time: 00:34:21
+// Time: 13:51:36
 
 #include <bits/stdc++.h>
 using namespace std;
-using ull = unsigned long long;
+using ll = long long;
+
 void ChatGptDeepSeek()
 {
     int n;
     cin >> n;
-    vector<int> s1(n + 1), s2(n + 1);
-    vector<char> c1(n + 1), c2(n + 1);
-    for (int i = 1; i <= n; i++)
-        cin >> c1[i] >> s1[i] >> c2[i] >> s2[i];
-    // 关于新增的人放哪边
-    //+ 操作与当前的人数无关的
-    // 所以看哪个后面乘的多就去哪边
-    vector<ull> p1(n + 2), p2(n + 2);
-    p1[n+1]=p2[n+1]=1;
-    for(int i=n;i>=1;i--){
-        p1[i]=p1[i+1],p2[i]=p2[i+1];
-        if(c1[i]=='x') p1[i]*=s1[i];
-        if(c2[i]=='x') p2[i]*=s2[i];
-    }
-    ull L = 1, R = 1;
-    for (int i = 1; i <= n; i++) {
-        ull x = 0;
-        if (c1[i] == 'x') {
-            x += (s1[i] - 1) * L;
+    ll x = 0, L = 1, R = 1;
+    while (n--) {
+        char o1, o2;
+        int x1, x2;
+        cin >> o1 >> x1 >> o2 >> x2;
+        if (o1 == 'x' && o2 == 'x') {
+            if (x1 > x2) {
+                L += x;
+                x = L * (x1 - 1) + R * (x2 - 1);
+            } else if (x1 < x2) {
+                R += x;
+                x = L * (x1 - 1) + R * (x2 - 1);
+            } else {
+                x = x * x1 + L * (x1 - 1) + R * (x2 - 1);
+            }
+        } else if (o1 == 'x') {
+            L += x;
+            x = L * (x1 - 1) + x2;
+        } else if (o2 == 'x') {
+            R += x;
+            x = R * (x2 - 1) + x1;
         } else {
-            x += s1[i];
+            x += x1 + x2;
         }
-        if (c2[i] == 'x') {
-            x += (s2[i] - 1) * R;
-        } else {
-            x += s2[i];
-        }
-        // cerr<<p1[i]<<" "<<p2[i]<<'\n';
-        if(p1[i+1]>=p2[i+1])
-            L+=x;
-        else R+=x;
     }
-    cout<<L+R<<'\n';
+    cout << L + R + x << '\n';
 }
-/*
-5
-x 3 x 3 ，3 1
-x 2 x 2 ，7 1
-+ 21 + 2，30 1
-x 2 x 3 ，62 1
-+ 41 x 3   
-*/
+
 int main()
 {
     ios::sync_with_stdio(false);
