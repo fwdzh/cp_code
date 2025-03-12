a2377f56c558da94b5565df7de48d4b5e12d85db
fwdzh <wu_yi_@outlook.com>
2025-03-12 23:52:48 +0800

    ...

--- XCPC/HDU-Spring2024/1/B.cpp
+++ XCPC/HDU-Spring2024/1/B.cpp
@@ -7,8 +7,7 @@ constexpr int mod = 998244353;
 ll ksm(ll a, ll b)
 {
     ll res = 1;
-    while (b)
-    {
+    while (b) {
         if (b & 1)
             res = res * a % mod;
         a = a * a % mod;
@@ -20,13 +19,38 @@ ll inv(ll x) { return ksm(x, mod - 2); }
 
 void ChatGptDeepSeek()
 {
-    int n, k;
-    cin >> n >> k;
-    vector<int> p(k + 1);
-    vector<bool> t(n + 1);
-    for (int i = 0; i <= k; i++)
-        cin >> p[i], t[p[i]] = true;
-    
+    int n, k, win;
+    cin >> n >> k >> win;
+    --win;
+    vector<int> p(k), f(k, 1);
+    for (int i = 0; i < k; i++)
+        cin >> p[i], --p[i];
+    sort(p.begin(),p.end());
+    ll ans = 1;
+    ll iv2 = inv(2);
+    while (!p.empty()) {
+        win >>= 1;
+        vector<int> b, g;
+        for (int i = 0; i < p.size(); i++) {
+            if ((p[i] >> 1) == win) {
+                ans = (ans * (1 - f[i])) % mod;
+                ans = (ans + mod) % mod;
+            } else {
+                b.push_back(p[i] >> 1);
+                if (i + 1 < p.size() && (p[i] >> 1) == (p[i + 1] >> 1)) {
+                    g.push_back(1LL * (f[i] + f[i + 1]) * iv2 % mod);
+                    i++;
+                } else if ((p[i] ^ 1) < n) {
+                    g.push_back(1LL * f[i] * iv2 % mod);
+                } else {
+                    g.push_back(f[i]);
+                }
+            }
+        }
+        p = b, f = g;
+        n = (n + 1) / 2;
+    }
+    cout << ans << '\n';
 }
 
 int main()
