5d9c064f44a78291a1c5a2c1cd76fb0001461914
fwdzh <wu_yi_@outlook.com>
2025-03-24 16:34:38 +0800

    ...

--- Codeforces/2090/D.cpp
+++ Codeforces/2090/D.cpp
@@ -1,91 +1,47 @@
-// Date: 2025-03-23
-// Time: 14:02:38
+// Date: 2025-03-24
+// Time: 10:14:47
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
 constexpr int N = 1e5;
-set<int> pri,wa;
-vector<bool> is(N + 1);
+vector<int> minp(N + 1);
 
 void ChatGptDeepSeek()
 {
-    pri=wa;
     int n;
     cin >> n;
-    ll sum = 0;
-    set<int> st;
-    for (int i = 1; i <= n; i++)
-        st.insert(i);
     vector<int> ans;
-    int cnt = 0;
-    for (cnt = 0; cnt < n / 3 - 1;)
+    int mid = n / 2;
+    for (int i = 0; i < mid; i++)
     {
-        if (pri.empty() || st.empty())
+        if (minp[mid - i] == mid - i)
         {
-            // cerr << "empty()!!!" << " " << cnt << '\n';
+            mid -= i;
             break;
         }
-        // if (rand() % 30 == 1)
-        // {
-        //     ans.push_back(*st.begin());
-        //     sum+=*st.begin
-        //     st.erase(st.begin());
-        // }
-        ll nxt = *pri.begin();
-        // cerr<<"Nxt : "<<nxt<<' ';
-        ll sz = ans.size() + 1;
-        bool find = true;
+        if (minp[mid + i] == mid + i)
         {
-            /* sum+x<=sz*nxt
-               sum+x>sz*(nxt-1)
-            */
-            auto i1 = st.upper_bound(sz * nxt - sum);
-            auto i2 = st.upper_bound(sz * (nxt - 1) - sum);
-            if (i1 == st.begin() || i2 == st.end())
-            {
-                find = false;
-            }
-            else
-            {
-                i1 = prev(i1);
-                if (*i2 <= *i1)
-                {
-                    ans.push_back(*i2);
-                    // cnt++;
-                    sum += *i2;
-                    // cerr << *i2 << '\n';
-                    st.erase(i2);
-                    cnt++;
-                }
-                else
-                    find = false;
-            }
-            if (!find)
-            {
-                // sum += *st.begin();
-                // st.erase(st.begin());
-
-                pri.erase(pri.begin());
-            }
+            mid += i;
+            break;
         }
     }
-    for(auto x:st)
-        ans.push_back(x);
-    for(auto x:ans)
-        cout<<x<<" ";
-        cout<<'\n';
-    // cerr << cnt << '\n';
+    cout << mid << " ";
+    int l = mid - 1, r = mid + 1;
+    while (l >= 1 || r <= n)
+    {
+        if (l >= 1)
+        {
+            cout << l << ' ';
+            l--;
+        }
+        if (r <= n)
+        {
+            cout << r << " ";
+            r++;
+        }
+    }
+    cout<<'\n';
 }
-/*
-2 1 3 4, 10/4 3
-2 1 3 4 5, 15/5 3
-2 1 3 4 5 6, 21/6 4
-2 1 3 4 5 6 7, 28/7 4
-2 1 3 4 5 8 7 6, 36/8 5
-2 1 3 4 5 8 7 6 9, 45/9 5
-2 1 3 4 5 8 7 6 9 10 , 55/10 6
-*/
 
 int main()
 {
@@ -94,19 +50,14 @@ int main()
     cout.tie(nullptr);
     for (int i = 2; i <= N; i++)
     {
-        if (is[i])
+        if (minp[i])
             continue;
-        // pri.push_back(i);
-        pri.insert(i);
+        minp[i] = i;
         if (1LL * i * i > N)
             continue;
-        for (long long j = i * i; j <= N; j += i)
-        {
-            is[j] = true;
-        }
+        for (int j = i * i; j <= N; j += i)
+            minp[j] = i;
     }
-    wa=pri;
-    // cerr << pri.size() << '\n';
     int T = 1;
     cin >> T;
     while (T--)
