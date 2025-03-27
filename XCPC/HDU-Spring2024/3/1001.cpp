24a997cfa0bd68613e28e2b16d2f724cc5fda6aa
fwdzh <wu_yi_@outlook.com>
2025-03-27 22:56:03 +0800

    ...

--- XCPC/HDU-Spring2024/3/1001.cpp
+++ XCPC/HDU-Spring2024/3/1001.cpp
@@ -3,26 +3,59 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+constexpr int mod = 998244353;
 
 void ChatGptDeepSeek()
 {
-    int n;
-    cin>>n;
-    vector<int>a(n+1),L(n+1);
-    for(int i=1;i<=n;i++) cin>>a[i];
-    for(int i=1;i<=n;i++) cin>>L[i];
     /*
-    只有奇数*奇数才会等于奇数
-    (3,0) 1,(3,1) 3 , 
-    (4,0) 1,(4,1) 4 , (4,2) 6
-    (5,0) 1,(5,1) 5 , (5,2) 10
-    (6,0) 1,(6,1) 6 , (6,2) 15 , (6,3) 20
-    (7,0) 1,(7,1) 7 , (7,2) 21 , (7,3) 35
-    (8,0) 1,(8,1) 8 ,       28 ,       56,    70
-          1,      9 ,       36 ,       84,    126
-          1,      10,       45 ,       120,   210    252
-          
+    101
+    101
+
+    2+1
     */
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    ll ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        int L;
+        cin >> L;
+        if (L >= a[i])
+        {
+            int x = 0;
+            for (int j = 30; j >= 0; j--)
+                x += (a[i] >> j & 1);
+            ans = ans * (1 << x) % mod;
+            continue;
+        }
+        int cnt = 0, ok = 1;
+        for (int j = 30; j >= 0; j--)
+        {
+            int x = a[i] >> j & 1;
+            int y = L >> j & 1;
+            if (y)
+            {
+                int bit = 0;
+                for (int k = j - 1; k >= 0; k--)
+                    bit += (a[i] >> k & 1);
+                cnt = (cnt + (1 << bit)) % mod;
+                if (x == 0)
+                {
+                    ok = 0;
+                    break;
+                }
+            }
+        }
+        if (ok)
+            cnt++;
+        ans = ans * cnt % mod;
+    }
+    cout << ans << '\n';
 }
 
 int main()
--- XCPC/HDU-Spring2024/3/1003.cpp
+++ XCPC/HDU-Spring2024/3/1003.cpp
@@ -3,18 +3,60 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
 
 void ChatGptDeepSeek()
 {
-    int n,m;
-    cin>>n>>m;
-    vector<int>a(m+1);
-    for(int i=1;i<=m;i++)
-        cin>>a[i];
-    vector<int>c(m+1),w(m+1);
-    for(int i=1;i<=m;i++) cin>>c[i];
-    for(int i=1;i<=m;i++) cin>>w[i];
-    
+    int n, m;
+    cin >> n >> m;
+    vector<ll> a(m + 1);
+    for (int i = 1; i <= m; i++)
+        cin >> a[i];
+    vector<vector<int>> c(n + 1, vector<int>(m + 1)), w(n + 1, vector<int>(m + 1));
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+            cin >> c[i][j];
+        for (int j = 1; j <= m; j++)
+            cin >> w[i][j];
+    }
+    vector<priority_queue<pii, vector<pii>, greater<>>> pq(m + 1);
+    queue<int> q;
+    vector<int> cnt(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (c[i][j] > a[j])
+            {
+                pq[j].push({c[i][j], i});
+                cnt[i]++;
+            }
+        }
+        if (!cnt[i])
+            q.push(i);
+    }
+    int ans = 0;
+    while (!q.empty())
+    {
+        ans++;
+        int k = q.front();
+        q.pop();
+        for (int i = 1; i <= m; i++)
+        {
+            a[i] += w[k][i];
+            while (!pq[i].empty() && a[i] >= pq[i].top().first)
+            {
+                auto [_, j] = pq[i].top();
+                pq[i].pop();
+                cnt[j]--;
+                if (!cnt[j])
+                    q.push(j);
+            }
+        }
+    }
+    cout << (ans == n ? "YES\n" : "NO\n");
 }
 
 int main()
