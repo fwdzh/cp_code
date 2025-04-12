23031a379f5766a24b001a1efe01ee7b97e189d7
fwdzh <wu_yi_@outlook.com>
2025-04-12 22:56:45 +0800

    ???

---
+++ Atcoder/ABC401/C.cpp
@@ -0,0 +1,72 @@
+// #define YUANSHEN
+#if defined(YUANSHEN)
+#include "/home/skadi/cp_code/template/debug.hpp"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#define dbg(...) 42
+#endif
+template <typename T1, typename T2>
+void cmin(T1 &x, const T2 &y)
+{
+    x = x < y ? x : y;
+}
+template <typename T1, typename T2>
+void cmax(T1 &x, const T2 &y)
+{
+    x = x > y ? x : y;
+}
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define fixset(x) fixed << setprecision(x)
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define ALL(x) (x).begin() + 1, (x).end()
+constexpr int INF = 1000000000;
+constexpr ll LNF = 1000000000000000000LL;
+
+void ChatGptDeepSeek() // Date: 2025-04-12
+{                      // Time: 21:09:41
+    int n, k;
+    cin >> n >> k;
+    vl a(n + 1);
+    if (k > n)
+    {
+        cout << "1\n";
+        return;
+    }
+    // cmin(k,n+1);
+    for (int i = 0; i < k; i++)
+        a[i] = 1;
+    ll s = k;
+
+    for (int i = k; i <= n; i++)
+    {
+        a[i] = s % INF;
+        s -= a[i - k];
+        s += a[i];
+        s %= INF;
+        s = (s + INF) % INF;
+    }
+    cout << a[n] << '\n';
+}
+
+int main()
+{
+#ifndef YUANSHEN
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        ChatGptDeepSeek();
+    return 0;
+}
---
+++ Atcoder/ABC401/D.cpp
@@ -0,0 +1,59 @@
+// #define YUANSHEN
+#if defined(YUANSHEN)
+#include "/home/skadi/cp_code/template/debug.hpp"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#define dbg(...) 42
+#endif
+template <typename T1, typename T2>
+void cmin(T1& x, const T2& y)
+{
+    x = x < y ? x : y;
+}
+template <typename T1, typename T2>
+void cmax(T1& x, const T2& y)
+{
+    x = x > y ? x : y;
+}
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define fixset(x) fixed << setprecision(x)
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define ALL(x) (x).begin() + 1, (x).end()
+constexpr int INF = 1000000000;
+constexpr ll LNF = 1000000000000000000LL;
+
+void ChatGptDeepSeek() // Date: 2025-04-12
+{                      // Time: 21:35:33 
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    string x=s;
+    for(int i=0;i<n;i++){
+        
+    }
+    
+}
+
+int main()
+{
+#ifndef YUANSHEN
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+    int T = 1;
+    cin >> T;
+    while (T--)
+        ChatGptDeepSeek();
+    return 0;
+}
