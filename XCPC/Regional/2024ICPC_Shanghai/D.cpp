ded2b5d6bf548bad4b290edca197143b97c9d793
fwdzh <wu_yi_@outlook.com>
2025-04-15 19:52:45 +0800

    ...

--- XCPC/Regional/2024ICPC_Shanghai/D.cpp
+++ XCPC/Regional/2024ICPC_Shanghai/D.cpp
@@ -1,80 +1,69 @@
-// #define YUANSHEN
-#if defined(YUANSHEN)
-#include "/home/skadi/cp_code/template/debug.hpp"
-#else
 #include <bits/stdc++.h>
 using namespace std;
-#define dbg(...) 42
-#endif
-template <typename T1, typename T2>
-void cmin(T1 &x, const T2 &y)
-{
-    x = x < y ? x : y;
-}
-template <typename T1, typename T2>
-void cmax(T1 &x, const T2 &y)
-{
-    x = x > y ? x : y;
-}
-using ll = long long;
-using ull = unsigned long long;
-using vi = vector<int>;
-using vl = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-#define fixset(x) fixed << setprecision(x)
-#define fi first
-#define se second
-#define sz(x) (int)(x).size()
-#define all(x) (x).begin(), (x).end()
-#define ALL(x) (x).begin() + 1, (x).end()
-constexpr int INF = 1000000000;
-constexpr ll LNF = 1000000000000000000LL;
 
-void ChatGptDeepSeek() // Date: 2025-04-14
-{                      // Time: 17:50:21
+void ChatGptDeepSeek() // Date: 2025-04-15
+{                      // Time: 15:05:45
     int n;
-    // 10101
     cin >> n;
-    string s;
-    cin >> s;
-    if (s[n - 1] == '0' && s[n - 2] == '0')
+    vector<char> s(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> s[i];
+    if (s[n] == '0' && s[n - 1] == '0')
     {
         cout << "Yes\n";
         return;
     }
     if (n == 3)
     {
-        if (s[2] == '1' || s[1] == '1')
-            cout << "No\n";
-        else
-            cout << "Yes\n";
+        cout << "No\n";
         return;
     }
-    if (s[n - 1] == '1' && s[n - 2] == '1')
+    for (int _ = 0; _ < 50; _++)
+        for (int i = 1; i + 4 <= n; i++)
+            if (s[i] == '1' && s[i + 1] == '1' && s[i + 2] == '0')
+                swap(s[i + 1], s[i + 2]);
+
+    /*
+    11101111
+    11011111
+
+     */
+    // if(s[n-3]=='0'&&s[n-2]=='1'&&n>4&&s[n-4]=='1')
+    //     swap(s[n-3],s[n-2]);
+    if (s[n - 2] == '1' && s[n - 3] == '1')
+        cout << "Yes\n";
+    else if (s[n - 3] == '1' && s[n - 2] == '0')
     {
-        if (s[n - 3] == '0' && s[n - 4] == '1')
-            cout << "Yes\n";
-        else
+        if (s[n - 1] == '0' && s[n] == '1')
             cout << "No\n";
-        return;
+        else
+            cout << "Yes\n";
     }
-    if (count(all(s), '1') >= 2)
+    else if (s[n - 3] == '0' && s[n - 2] == '0')
+        cout << "No\n";
+    else if (n > 4 && s[n - 4] == '1')
         cout << "Yes\n";
     else
         cout << "No\n";
-    // 110-> 010 ，前面有1就ok
-    // 101-> 110 , 前面若有1 1110-> 1010 ok
-    // 111->
+    // 1110 ->1010->1100
+    // 1101 ->1110->1110->1010->1100
+    // 1111 ->1011->1101->1110->1110->1010->1100
+    // 1010 ->1100
+    // 1011 ->1101->1110->1110->1010->1100
+    // 1001 x
+    // 10101
+    // 10110
+    // 10101
+    // 10110
+    // 11010
+    //
 }
 
 int main()
 {
-#ifndef YUANSHEN
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-#endif
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int T = 1;
     cin >> T;
     while (T--)
