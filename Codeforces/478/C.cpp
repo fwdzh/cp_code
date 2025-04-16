6fe051940ae72e025ce7d62e8bbcc371840ee7e4
fwdzh <wu_yi_@outlook.com>
2025-04-16 11:21:37 +0800

    CF478C 1800 感觉该1200

---
+++ Codeforces/478/C.cpp
@@ -0,0 +1,63 @@
+// #define YUANSHEN
+#if defined(YUANSHEN)
+#include "/home/skadi/cp_code/template/debug.hpp"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#define dbg(...) 42
+#endif
+template <typename T1, typename T2>
+void cmin(T1& x, const T2& y)
+{
+    x = x < y ? x : y;
+}
+template <typename T1, typename T2>
+void cmax(T1& x, const T2& y)
+{
+    x = x > y ? x : y;
+}
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define fixset(x) fixed << setprecision(x)
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define ALL(x) (x).begin() + 1, (x).end()
+constexpr int INF = 1000000000;
+constexpr ll LNF = 1000000000000000000LL;
+
+void ChatGptDeepSeek() // Date: 2025-04-16
+{                      // Time: 11:10:02 
+    vl a(3);
+    cin>>a[0]>>a[1]>>a[2];
+    sort(all(a),greater<ll>());
+    if(a[0]>=(a[1]+a[2])*2){
+        cout<<a[1]+a[2]<<'\n';
+    }else
+        cout<<(a[0]+a[1]+a[2])/3<<'\n';
+    /* 
+    9 4 3
+    
+    
+    */
+    
+}
+
+int main()
+{
+#ifndef YUANSHEN
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        ChatGptDeepSeek();
+    return 0;
+}
