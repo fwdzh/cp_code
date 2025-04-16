a792682ca9ae0be41aee9bab03fa54db8516529a
fwdzh <wu_yi_@outlook.com>
2025-04-16 10:51:06 +0800

    CF1458A 1600 简单gcd 四分钟就行

---
+++ Codeforces/1458/sol.cpp
@@ -0,0 +1,60 @@
+// #define YUANSHEN
+#if defined(YUANSHEN)
+#include "/home/skadi/cp_code/template/debug.hpp"
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#define dbg(...) 42
+#endif
+template <typename T1, typename T2>
+void cmin(T1& x, const T2& y)
+{
+    x = x < y ? x : y;
+}
+template <typename T1, typename T2>
+void cmax(T1& x, const T2& y)
+{
+    x = x > y ? x : y;
+}
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define fixset(x) fixed << setprecision(x)
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define ALL(x) (x).begin() + 1, (x).end()
+constexpr int INF = 1000000000;
+constexpr ll LNF = 1000000000000000000LL;
+
+void ChatGptDeepSeek() // Date: 2025-04-16
+{                      // Time: 10:46:42 
+    int n,m; cin>>n>>m;
+    vl a(n),b(m);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<m;i++) cin>>b[i];
+    sort(all(a));
+    ll g=0;
+    for(int i=1;i<n;i++) g=__gcd(g,a[i]-a[i-1]);
+    for(int j=0;j<m;j++){
+        cout<<__gcd(g,a[0]+b[j])<<" \n"[j+1==m];
+    }
+}
+
+int main()
+{
+#ifndef YUANSHEN
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        ChatGptDeepSeek();
+    return 0;
+}
