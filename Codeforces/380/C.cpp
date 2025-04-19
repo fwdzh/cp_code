f71b20e1eb0ac08eabb56ed64aaf06906f683e6f
fwdzh <wu_yi_@outlook.com>
2025-04-20 02:01:25 +0800

    CF380C 2000 线段树

--- Codeforces/380/C.cpp
+++ Codeforces/380/C.cpp
@@ -31,9 +31,52 @@ using pll = pair<ll, ll>;
 constexpr int INF = 1000000000;
 constexpr ll LNF = 1000000000000000000LL;
 
+string s;
+#define ls p<<1
+#define rs p<<1|1
+#define mi ((l+r)>>1)
+
+constexpr int N = int(1e6)+5;
+struct node{
+    int l,r;
+};
+node tr[N<<2];
+
+node merge(node x,node y){
+    node res;
+    res.l=x.l+y.l-min(x.l,y.r);
+    res.r=x.r+y.r-min(x.l,y.r);
+    return res;
+}
+void build(int p,int l,int r){
+    if(l==r){
+        if(s[l]=='(') tr[p]={1,0};
+        else tr[p]={0,1};
+        return;
+    }
+    build(ls,l,mi),build(rs,mi+1,r);
+    tr[p]=merge(tr[ls],tr[rs]);
+}
+node query(int p,int l,int r,int lx,int rx)
+{
+    if(l>=lx&&r<=rx) return tr[p];
+    node res{0,0};
+    if(lx<=mi) res=merge(res,query(ls,l,mi,lx,rx));
+    if(rx>mi) res=merge(res,query(rs,mi+1,r,lx,rx));
+    return res;
+}
 void ChatGptDeepSeek() // Date: 2025-04-16
 {                      // Time: 11:32:28 
-    
+    cin>>s;
+    int n=sz(s);
+    s=" "+s;
+    build(1,1,n);
+    int m; cin >> m;
+    while(m--){
+        int l,r; cin>>l>>r;
+        node res=query(1,1,n,l,r);
+        cout<<r-l+1-res.l-res.r<<'\n';
+    }
 }
 
 int main()
@@ -44,7 +87,7 @@ int main()
     cout.tie(0);
 #endif
     int T = 1;
-    cin >> T;
+    // cin >> T;
     while (T--)
         ChatGptDeepSeek();
     return 0;
