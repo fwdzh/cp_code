bc3fbdfd0b5c11cfb78279bef06d3efd9214c247
fwdzh <wu_yi_@outlook.com>
2025-04-20 10:08:42 +0800

    ...

--- Codeforces/2096/D.cpp
+++ Codeforces/2096/D.cpp
@@ -1,18 +1,18 @@
 // #define YUANSHEN
 #if defined(YUANSHEN)
-#include "/home/skadi/cp_code/template/debug.hpp"
+#include "C:/cp_code/template/debug.hpp"
 #else
 #include <bits/stdc++.h>
 using namespace std;
 #define dbg(...) 42
 #endif
 template <typename T1, typename T2>
-void cmin(T1 &x, const T2 &y)
+void cmin(T1& x, const T2& y)
 {
     x = x < y ? x : y;
 }
 template <typename T1, typename T2>
-void cmax(T1 &x, const T2 &y)
+void cmax(T1& x, const T2& y)
 {
     x = x > y ? x : y;
 }
@@ -30,38 +30,17 @@ using pll = pair<ll, ll>;
 #define ALL(x) (x).begin() + 1, (x).end()
 constexpr int INF = 1000000000;
 constexpr ll LNF = 1000000000000000000LL;
-mt19937_64 rng(time(NULL));
-ll rnd(ll l, ll r)
-{
-    uniform_int_distribution<ll> dis(l, r);
-    return dis(rng);
-}
-void ChatGptDeepSeek() // Date: 2025-04-19
-{                      // Time: 23:37:48
-    int n;
-    cin >> n;
-    vector<pii> p(n), q;
-    set<pii> st;
-    for (int i = 0; i < n; i++)
-    {
-        int x, y;
-        cin >> x >> y;
-        p[i] = {x, y};
-        st.insert({x, y});
-    }
-    auto find = [&](int x, int y)
-    {
-        if (st.contains({x, y + 1}) && st.contains({x + 1, y}) && st.contains({x + 1, y - 1}))
-            p.push_back({x, y});
-    };
-    for (int i = 0; i < n; i++)
-    {
-        auto [x, y] = p[i];
-        find(x - 1, y), find(x + 1, y), find(x, y - 1), find(x, y + 1);
-    }
 
-    auto [x, y] = p[rnd(0, sz(p) - 1)];
-    cout << x << " " << y << '\n';
+void ChatGptDeepSeek() // Date: 2025-04-20
+{                      // Time: 10:01:56 
+    int n;
+    cin>>n;
+    int X=0,Y=0;
+    while(n--){
+        int x,y; cin>>x>>y;
+        X^=x,Y^=(x+y);
+    }
+    cout<<X<<" "<<Y-X<<'\n';
 }
 
 int main()
