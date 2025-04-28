d32992751d3b2cca4912679417146fbd2bedf3db
fwdzh <wu_yi_@outlook.com>
2025-04-29 01:11:43 +0800

    update on 2025-04-29 01:11:43

--- push.sh
+++ push.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+  msg="update on $(date '+%Y-%m-%d %H:%M:%S')"
+else
+  msg=""$1" $(date '+%Y-%m-%d %H:%M:%S')"
+fi
+
+# echo "$msg"
 git add .
-git commit -m "$1"
-git push
+git commit -m "$msg"
+git push && git push github master
