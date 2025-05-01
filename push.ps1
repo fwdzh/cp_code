86ef53639f5e3995ece0ed6a79e96155c5d69ec6
fwdzh <wu_yi_@outlook.com>
2025-05-01 15:36:15 +0800

    以后再也不用每次push都打三行代码了。。。 真爽吧 2025-05-01 15:36:10

---
+++ push.ps1
@@ -0,0 +1,21 @@
+$time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+if ($args.Count -eq 0) {
+    $msg = "update on $time"
+} else {
+    $msg = "$($args -join ' ') $time"
+}
+
+git add .
+git commit -m "$msg"
+git push
+git push gitee master
+
+Set-Location ..\blog
+$sss = "update on $time"
+git add .
+# git commit -m "$sss"
+git commit -m "$msg"
+git push
+git push gitee main
+Set-Location ..\blog
