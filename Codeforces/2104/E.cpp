05697ed9a1bbffdf9fa270ab22245d4a7451c853
fwdzh <wu_yi_@outlook.com>
2025-05-29 11:18:55 +0800

    CF2104E 1700 确实挺简单 2025-05-29 11:18:55

--- Codeforces/2104/E.cpp
+++ Codeforces/2104/E.cpp
@@ -1,68 +1,100 @@
-#include<bits/stdc++.h>
+// #define YUANSHEN
+#if defined(YUANSHEN)
+#include "C:/codes/cp_code/template/debug.hpp"
+#else
+#include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+#define dbg(...) 42
+#endif
 
-void solve()
+#define rep(N) for(int i = 1; i <= N; i++)
+
+template <typename T1, typename T2>
+void cmin(T1& x, const T2& y)
+{
+    x = x < y ? x : y;
+}
+template <typename T1, typename T2>
+void cmax(T1& x, const T2& y)
+{
+    x = x > y ? x : y;
+}
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+template <class T>
+using vc = vector<T>;
+
+#define fixset(x) fixed << setprecision(x)
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define ALL(x) (x).begin() + 1, (x).end()
+constexpr int INF = 1000000000;
+constexpr ll LNF = 1000000000000000000LL;
+
+// Date: 2025-05-29
+// Time: 10:50:06
+void ChatGptDeepSeek()
 {
     int n, k;
     cin >> n >> k;
-    
-    vector<char> s(n + 1);
-    vector<vector<int>> pre(n + 1, vector<int> (k));
-    vector<vector<int>> idx(n + 1, vector<int> (k, n + 1));
+    string s;
+    cin >> s;
+    s = " " + s;
+    vc<vi> pre(n + 1, vi(k));
+    vc<vi> nxt(n + 1, vi(k));
     for(int i = 1; i <= n; i++){
         pre[i] = pre[i - 1];
-        cin >> s[i];
         pre[i][s[i] - 'a']++;
     }
+    vi lst(k, n + 1);
     for(int i = n; i >= 1; i--){
-        if(i < n) idx[i] = idx[i + 1];
-        idx[i][s[i] - 'a'] = i;
+        nxt[i] = lst;
+        lst[s[i] - 'a'] = i;
     }
-    
-    vector<int> ans(n + 2, n + 1);
-    ans[n + 1] = 0;
+    nxt[0] = lst;
+    vi ans(n + 1, n + 1);
     for(int i = n; i >= 1; i--){
-        for(int j = 0; j < k; j++)
-            ans[i] = min(ans[i], pre[n][j] - pre[i - 1][j]);
-        // cerr << ans[i] << " \n"[i==1];
+        for(int j = 0; j < k; j++){
+            if(nxt[i][j] > n)
+                ans[i] = 1;
+            else
+                cmin(ans[i], ans[nxt[i][j]] + 1);
+        }
     }
-    int q; cin >> q;
-    while (q--)
-    {
+    auto ask = [&](){
         string t;
         cin >> t;
-        if(t.size() > n){
-            cout << "0\n";
-            continue;
-        }
-        int j = 1;
-        bool ok = false;
-        for(int i = 0; i < t.size(); i++){
+        int j = 0;
+        for(int i = 0; i < sz(t); i++){
+            j = nxt[j][t[i] - 'a'];
             if(j > n){
-                ok = true;
-                break;
-            }
-            // cerr << j << '\n';
-            j = idx[j][t[i] - 'a'] + 1;
-            if(j > n + 1){
-                ok = true;
-                break;
+                cout << "0\n";
+                return;
             }
         }
-        
-        if(ok) cout << "0\n";
-        else cout << ans[j] + 1 <<'\n';
+        cout << ans[j] << '\n';
+    };
+    int q; cin >> q;
+    while(q--){
+        ask();
     }
-    
 }
+
 int main()
 {
-    // ios::sync_with_stdio(false);
-    // cin.tie(nullptr),cout.tie(nullptr);
-    int t = 1; 
-    // cin >> t;
-    while(t--)
-        solve();
+#ifndef YUANSHEN
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+#endif
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        ChatGptDeepSeek();
     return 0;
 }
