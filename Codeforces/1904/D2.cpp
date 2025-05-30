3437eb41bc98ddde17e8eec8ae99de66013aeecd
fwdzh <wu_yi_@outlook.com>
2025-05-30 15:13:50 +0800

    CF1904D2 水题 只用查询区间MIN MAX就行 2025-05-30 15:13:50

--- Codeforces/1904/D2.cpp
+++ Codeforces/1904/D2.cpp
@@ -42,17 +42,26 @@ constexpr ll LNF = 1000000000000000000LL;
 struct ST{
     vector<vector<int>> MIN;
     vector<vector<int>> MAX;
-    ST(vector<int> &a){
+    ST(vector<int> &a, vector<int> &b){
         int n = a.size();
         MIN = MAX = vector<vector<int>> (n, vector<int> (__lg(n) + 1));
         n--;
         for(int i = 1; i <= n; i++)
-            MIN[i][0] = MAX[i][0] = a[i];
-        for(int k = 1; k <= __lg(n); k++){
+            MAX[i][0] = a[i], MIN[i][0] = b[i];
+        for(int k = 1; (1 << k) <= n; k++){
             for(int s = 1; s + (1 << k) <= n + 1; s++){
-                MIN[s][k] = min(MIN[s][k - 1], MIN[s + (1 << k) - 1][k - 1]);
+                MIN[s][k] = min(MIN[s][k - 1], MIN[s + (1 << (k - 1))][k - 1]);
+                MAX[s][k] = max(MAX[s][k - 1], MAX[s + (1 << (k - 1))][k - 1]);
             }
         }
+    }
+    int queryMin(int l, int r){
+        int k = __lg(r - l + 1);
+        return min(MIN[l][k], MIN[r - (1 << k) + 1][k]);
+    }
+    int queryMax(int l, int r){
+        int k = __lg(r - l + 1);
+        return max(MAX[l][k], MAX[r - (1 << k) + 1][k]);
     }
 };
 
@@ -63,21 +72,41 @@ void ChatGptDeepSeek()
     vector<int> a(n + 1), b(n + 1);
     for(int i = 1; i <= n; i++) cin >> a[i];
     for(int i = 1; i <= n; i++) cin >> b[i];
-    vector<ll> pre(n + 1);
     /*
-    不对不对。。。 how nlog n
-    意思就是说 要满足找的值是区间max 还不要影响后面的操作。。。
-    但是必须要往左边或者右边找 但是我们能控制修改的顺序
+    <think>
+        向左找 X:
+            X 必须是 a[j, i] 之间的最大值， 小于等于 b[j, i] 之间的最小值
+        向右同理
 
-    向左:
-        x 必须之前是 a[1, i - 1] 的区间 max
-        也要小于等于 b[1, i - 1] 的区间 min
-
-    向右:
-        好像是和上面一样的吧（）
-    
-    不管了 先试试
+        有没有其他限制呢？
+        不满足这个条件，一定不行
+        满足这个条件，一定可以吗？
+    </think>
     */
+    vc<bool> ok(n + 1);
+    ST st(a, b);
+    vi l(n + 1), r(n + 1);
+    for(int i = 1; i <= n; i++){
+        l[a[i]] = i;
+        if(l[b[i]]){
+            // cerr << format("[l, i] : {} {}, MAX: {}, MIN: {}\n", l[b[i]], i, st.queryMax(l[b[i]], i), st.queryMin(l[b[i]], i));
+            if(st.queryMax(l[b[i]], i) == b[i] && st.queryMin(l[b[i]], i) == b[i])
+                ok[i] = true;
+        }
+    }
+    for(int i = n; i >= 1; i--){
+        r[a[i]] = i;
+        if(r[b[i]]){
+            if(st.queryMax(i, r[b[i]]) == b[i] && st.queryMin(i, r[b[i]]) == b[i])
+                ok[i] = true;
+        }
+        if(!ok[i]){
+            // cout << i << '\n';
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
 }
 
 int main()
