c60fdf290971fb27eae7d45fb4e197586b62fa25
fwdzh <wu_yi_@outlook.com>
2025-10-20 16:03:51 +0800

    update on 2025-10-20 16:03:51

--- Codeforces/2154/D.cpp
+++ Codeforces/2154/D.cpp
@@ -20,35 +20,37 @@ void solve()
             self(self, v, u);
         }
     };
-    init(init, 1, 0);
+    init(init, n, 0);
     vector<vector<int>> ans;
     vector<vector<int>> leaves(2, vector<int> ());
     for(int i = 1; i < n; i++)
-        if(ch[i] == 0) leaves[i & 1].push_back(i);
+        if(ch[i] == 0) leaves[dep[i] & 1].push_back(i);
     auto dfs = [&](auto &&self, int col) -> void {
-        if(leaves[0].empty() && leaves[1].empty()) return;
+
         if(!leaves[col ^ 1].empty()){
             int k = leaves[col ^ 1].back();
-            leaves.pop_back();
+            leaves[col ^ 1].pop_back();
             ans.push_back({2, k});
-            if(--ch[fa[k]] == 0)
+            if(--ch[fa[k]] == 0 && fa[k] != n)
                 leaves[col].push_back(fa[k]);
         }
         ans.push_back({1});
+        if(leaves[0].empty() && leaves[1].empty()) return;
         self(self, col ^ 1);
     };
-    dfs(dfs, 1);
+    dfs(dfs, dep[1] & 1);
     cout << ans.size() << "\n";
     for(auto v : ans){
         for(auto x : v) cout << x << " ";
         cout << "\n";
-    } 
-
+    }
 }
 int main()
 {
+// #define LOCAL
 #ifdef LOCAL
     freopen("1.in", "r", stdin);
+    freopen("1.out", "w", stdout);
 #endif
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t; cin >> t;
